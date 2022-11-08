db965f0eb2d3a5eacf5e561b9d0df37111313357
Poor-Richard <35991885+Poor-Richard@users.noreply.github.com>
2022-11-08 13:26:21 +0800

    init

---
+++ spring-cloud-alibaba-nacos-config/src/main/java/com/pzhu/spring/cloud/alibaba/nacos/config/NacosConfigApplication.java
@@ -0,0 +1,20 @@
+package com.pzhu.spring.cloud.alibaba.nacos.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * nacos配置中心
+ * @Author PoorRichard
+ * @Date  15:02
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+
+}
